9b9860fa8401000f192aafabb26d305bbb2a4a4a
renzon <renzo@gmail.com>
2014-03-28 10:39:30 -0300

    Generating screens for payment list and payment detail

--- project_template/backend/src/web/home.py
+++ project_template/backend/src/web/home.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from tekton import router
-from web.pagseguro import admin, form
+from web.pagseguro import admin, form, pagamento
 
 
 def index(_write_tmpl):
     dct = {'admin_form': router.to_path(admin.index),
-           'payment_form': router.to_path(form.index)}
+           'payment_form': router.to_path(form.index),
+           'payment_list': router.to_path(pagamento.listar)}
     _write_tmpl('templates/home.html', dct)
--- src/gaepagseguro/model.py
+++ src/gaepagseguro/model.py
@@ -4,6 +4,10 @@ from google.appengine.ext import ndb
 from gaegraph.model import Node, Arc
 from ndbext.property import SimpleCurrency, IntegerBounded
 
+STATUS_CREATED = 'CREATED'
+STATUS_SENT_TO_PAGSEGURO = 'SENT_TO_PAGSEGURO'
+STATUSES = [STATUS_CREATED, STATUS_SENT_TO_PAGSEGURO]
+
 
 class PagSegAccessData(Node):
     email = ndb.StringProperty(required=True, indexed=False)
@@ -12,10 +16,12 @@ class PagSegAccessData(Node):
 
 class PagSegOrder(Node):
     # code returned from Pagseguro after payment generation
-    code=ndb.StringProperty()
+    code = ndb.StringProperty()
+    status = ndb.StringProperty(default=STATUS_CREATED, choices=STATUSES)
+
     @classmethod
-    def query_by_code(cls,code):
-        return cls.query(cls.code==code)
+    def query_by_code(cls, code):
+        return cls.query(cls.code == code)
 
 
 class OriginToPagSegOrder(Arc):
@@ -34,6 +40,9 @@ class PagSegItem(Node):
     description = ndb.TextProperty(required=True, indexed=False)
     price = SimpleCurrency(required=True, indexed=False)
     quantity = IntegerBounded(required=True, lower=1, indexed=False)
+
+    def total(self):
+        return self.price * self.quantity
 
 
 class PagSegOrderToItem(Arc):
