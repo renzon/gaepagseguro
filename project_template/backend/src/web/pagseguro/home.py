2e505d0501d51de13a5fa238f3498b0b409b3074
renzon <renzo@gmail.com>
2014-04-07 21:37:06 -0300

    #14 PagSeguro return page

--- project_template/backend/src/web/pagseguro/home.py
+++ project_template/backend/src/web/pagseguro/home.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from gaepagseguro import facade
+from gaepagseguro.model import STATUS_CREATED, STATUS_SENT_TO_PAGSEGURO, STATUS_ANALYSIS, STATUS_ACCEPTED, \
+    STATUS_AVAILABLE
 
 
-def index(*args, **kwargs):
-    pass
+def _to_msg(status):
+    processing = 'PROCESSING'
+    ok = 'OK'
+    dct = {STATUS_CREATED: processing,
+           STATUS_SENT_TO_PAGSEGURO: processing,
+           STATUS_ANALYSIS: processing,
+           STATUS_ACCEPTED: ok,
+           STATUS_AVAILABLE: ok, }
+    return dct.get(status, 'PROBLEM')
+
+
+def index(_write_tmpl, transaction_id):
+    payment = facade.payment_detail(transaction_id).execute().result
+
+    _write_tmpl('templates/gaepagseguro/payment_return.html',
+                {'payment': payment, 'msg': _to_msg(payment.status)})
