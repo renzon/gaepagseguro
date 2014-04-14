09d6253f574560eaa4a7bd82103feee84a7198b4
renzon <renzon@gmail.com>
2014-04-14 20:40:48 -0300

    close #11 the previous commits regarding #14 were wrong.

--- project_template/backend/src/web/pagseguro/api.py
+++ project_template/backend/src/web/pagseguro/api.py
@@ -13,26 +13,37 @@ from web.pagseguro import home
 
 
 
-def gerar_pagamento(_handler, client_name, client_email, descriptions, quantities, prices,
+def gerar_pagamento(_handler, _write_tmpl, client_name,
+                    client_email, description, quantity, price,
                     street, number, quarter, postalcode, town, state, complement="Sem Complemento", country="BRA",
                     currency='BRL'):
     #saving mocking objects
     owner_key = FindOrCreateUserCmd().execute().result
-    items_references = [OrderItem() for i in xrange(len(descriptions))]
+    items_references = [OrderItem() for i in xrange(len(description))]
     ndb.put_multi(items_references)
 
     #generating parameters
-    items = [facade.create_item(ref, description, price, int(quantity)) for ref, description, price, quantity
-             in izip(items_references, descriptions, prices, quantities)]
+    items = [facade.create_item(ref, description, price, quantity) for ref, description, price, quantity
+             in izip(items_references, description, price, quantity)]
     address = facade.address(street, number, quarter, postalcode, town, state, complement, country)
     return_path = 'https://gaepagseguro.appspot.com%s' % router.to_path(home.index)
 
     #generating payment
     cmd = facade.payment(return_path, client_name, client_email, owner_key, items, address, currency=currency)
-    code = cmd.execute().result.code
-
-    # Redirecting to pag seguro site
-    if code:
+    cmd.execute()
+    if cmd.errors:
+        dct = {'client_name': client_name,
+               'client_email': client_email,
+               'items': items,
+               'street': street, 'number': number,
+               'quarter': quarter, 'postalcode': postalcode,
+               'town': town, 'state': state, 'complement': complement,
+               'payment_path': router.to_path(gerar_pagamento),
+               'errors': cmd.errors}
+        _write_tmpl('templates/gaepagseguro/payment_form.html', dct)
+    else:
+        # Redirecting to pag seguro site
+        code = cmd.result.code
         _handler.redirect(facade.pagseguro_url(code))
 
 
